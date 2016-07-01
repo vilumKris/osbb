407d2cf0442f7ad0040ec9186f96ebad7c592d61
Aska <aska.fed@gmail.com>
2016-07-01 17:17:47 +0300

    fix errors in hibernate versions, add demo main class

---
+++ persistences/src/main/java/JpaDemo.java
@@ -0,0 +1,24 @@
+import dao.impl.StaffDAOImpl;
+import model.StaffEntity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * Created by Aska on 01.07.2016.
+ */
+public class JpaDemo {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUnit");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        StaffDAOImpl staffDAO = new StaffDAOImpl();
+        StaffEntity staffEntity = new StaffEntity();
+        staffEntity.setIdStaff(10);
+        staffDAO.save(staffEntity);
+        System.out.println(staffDAO.findAll());
+
+    }
+
+}
---
+++ persistences/src/main/java/dao/impl/StaffDAOImpl.java
@@ -0,0 +1,106 @@
+package dao.impl;
+
+import dao.StaffDAO;
+import model.StaffEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+
+import java.util.List;
+
+/**
+ * Created by Aska on 01.07.2016.
+ */
+public class StaffDAOImpl implements StaffDAO {
+    @Override
+    public List<StaffEntity> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<StaffEntity> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public List<StaffEntity> findAll(Iterable<Integer> iterable) {
+        return null;
+    }
+
+    @Override
+    public <S extends StaffEntity> List<S> save(Iterable<S> iterable) {
+        return null;
+    }
+
+    @Override
+    public StaffEntity findOne(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public boolean exists(Integer integer) {
+        return false;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void delete(Integer integer) {
+
+    }
+
+    @Override
+    public void delete(StaffEntity staffEntity) {
+
+    }
+
+    @Override
+    public void delete(Iterable<? extends StaffEntity> iterable) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends StaffEntity> S saveAndFlush(S s) {
+        return null;
+    }
+
+    @Override
+    public void deleteInBatch(Iterable<StaffEntity> iterable) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    @Override
+    public StaffEntity getOne(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public Page<StaffEntity> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends StaffEntity> S save(S s) {
+        System.out.println("privet from save");
+        return null;
+    }
+}
