8fa926b78acb670395adb81217102d8d7f611eef
Cavayman <cavayman@gmail.com>
2016-07-04 15:03:41 +0300

    renamed Persistences on Persistence

--- persistences/src/main/java/dao/OsbbDAO.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import model.OsbbEntity;
-
-/**
- * Created by Roman on 02.07.2016.
- */
-public interface OsbbDAO extends  GenericDAO<OsbbEntity, Integer>{
-}
--- persistences/src/main/java/dao/VoteDAO.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import model.VoteEntity;
-
-/**
- * Created by Roman on 02.07.2016.
- */
-public interface VoteDAO extends GenericDAO<VoteEntity, Integer> {
-}
