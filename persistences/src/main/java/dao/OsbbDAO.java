9e6d7f122057cb7f6464ce0c464c4f7dd237bc77
butaroman <butaromanjob@gmail.com>
2016-07-04 14:24:36 +0300

    delete trash

--- persistences/src/main/java/dao/OsbbDAO.java
+++ persistences/src/main/java/dao/OsbbDAO.java
@@ -1,10 +1,9 @@
 package dao;
 
 import model.OsbbEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Created by Roman on 02.07.2016.
  */
-public interface OsbbDAO extends  JpaRepository<OsbbEntity, Integer>{
+public interface OsbbDAO extends  GenericDAO<OsbbEntity, Integer>{
 }
--- persistences/src/main/java/dao/impl/OsbbDAOImpl.java
+++ persistences/src/main/java/dao/impl/OsbbDAOImpl.java
@@ -2,13 +2,9 @@ package dao.impl;
 
 import dao.OsbbDAO;
 import model.OsbbEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import util.EntityUtil;
 
 import javax.persistence.EntityManager;
-import java.util.List;
 
 /**
  * Created by Roman on 02.07.2016.
@@ -26,88 +22,4 @@ public class OsbbDAOImpl implements OsbbDAO {
         return s;
     }
 
-    @Override
-    public OsbbEntity findOne(Integer integer) {
-        return null;
-    }
-
-    @Override
-    public boolean exists(Integer integer) {
-        return false;
-    }
-
-    @Override
-    public List<OsbbEntity> findAll() {
-        return null;
-    }
-
-    @Override
-    public List<OsbbEntity> findAll(Sort sort) {
-        return null;
-    }
-
-    @Override
-    public Page<OsbbEntity> findAll(Pageable pageable) {
-        return null;
-    }
-
-    @Override
-    public List<OsbbEntity> findAll(Iterable<Integer> iterable) {
-        return null;
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public void delete(Integer integer) {
-
-    }
-
-    @Override
-    public void delete(OsbbEntity osbbEntity) {
-
-    }
-
-    @Override
-    public void delete(Iterable<? extends OsbbEntity> iterable) {
-
-    }
-
-    @Override
-    public void deleteAll() {
-
-    }
-
-    @Override
-    public void flush() {
-
-    }
-
-    @Override
-    public void deleteInBatch(Iterable<OsbbEntity> iterable) {
-
-    }
-
-    @Override
-    public void deleteAllInBatch() {
-
-    }
-
-    @Override
-    public OsbbEntity getOne(Integer integer) {
-        return null;
-    }
-
-    @Override
-    public <S extends OsbbEntity> S saveAndFlush(S s) {
-        return null;
-    }
-
-    @Override
-    public <S extends OsbbEntity> List<S> save(Iterable<S> iterable) {
-        return null;
-    }
 }
